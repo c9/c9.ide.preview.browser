21304a8408653f09a611e569473812ad2c88d430
Lennart Kats <lclkats@gmail.com>
2014-05-06 11:31:12 +0200

    Apply scripts/apply-conventions.sh
    Applying the code conventions as discussed in the company meeting 2nd of May 2014

--- browser.js
+++ browser.js
@@ -7,40 +7,40 @@ define(function(require, exports, module) {
     return main;
 
     function main(options, imports, register) {
-        var Previewer    = imports.Previewer;
-        var tabManager   = imports.tabManager;
-        var preview      = imports.preview;
-        var PostMessage  = imports["remote.PostMessage"];
-        var CSSDocument  = imports.CSSDocument;
+        var Previewer = imports.Previewer;
+        var tabManager = imports.tabManager;
+        var preview = imports.preview;
+        var PostMessage = imports["remote.PostMessage"];
+        var CSSDocument = imports.CSSDocument;
         var HTMLDocument = imports.HTMLDocument;
-        var JSDocument   = imports.JSDocument;
+        var JSDocument = imports.JSDocument;
         
-        // var join        = require("path").join;
-        // var dirname     = require("path").dirname;
+        // var join = require("path").join;
+        // var dirname = require("path").dirname;
         
         /***** Initialization *****/
         
         var plugin = new Previewer("Ajax.org", main.consumes, {
-            caption  : "Browser",
-            index    : 10,
-            divider  : true,
-            selector : function(path){
+            caption: "Browser",
+            index: 10,
+            divider: true,
+            selector: function(path) {
                 return path.match(/(?:\.html|\.htm|\.xhtml)$|^https?\:\/\//);
             }
         });
         
         var BASEPATH = preview.previewUrl;
-        var counter  = 0;
+        var counter = 0;
         
         /***** Methods *****/
         
-        function calcRootedPath(url){
+        function calcRootedPath(url) {
             if (url.substr(0, BASEPATH.length) == BASEPATH)
                 return url.substr(BASEPATH.length);
             return url;
         }
         
-        function cleanIframeSrc(src){
+        function cleanIframeSrc(src) {
             return src
                 .replace(/_c9_id=\w+\&_c9_host=.*?(?:\&|$)/, "")
                 .replace(/[\?\&]$/, "");
@@ -50,11 +50,11 @@ define(function(require, exports, module) {
         
         plugin.on("load", function(){
         });
-        plugin.on("documentLoad", function(e){
-            var doc     = e.doc;
+        plugin.on("documentLoad", function(e) {
+            var doc = e.doc;
             var session = doc.getSession();
-            var tab     = doc.tab;
-            var editor  = e.editor;
+            var tab = doc.tab;
+            var editor = e.editor;
             
             if (session.iframe) {
                 session.editor = editor;
@@ -66,7 +66,7 @@ define(function(require, exports, module) {
             iframe.setAttribute("nwfaketop", true);
             iframe.setAttribute("nwdisable", true);
             
-            iframe.style.width  = "100%";
+            iframe.style.width = "100%";
             iframe.style.height = "100%";
             iframe.style.border = 0;
             iframe.style.backgroundColor = "rgba(255, 255, 255, 0.88)";
@@ -76,9 +76,9 @@ define(function(require, exports, module) {
                 
                 var path = calcRootedPath(cleanIframeSrc(iframe.src));
                 
-                tab.title   = 
+                tab.title = 
                 tab.tooltip = "[B] " + path;
-                session.lastSrc  = iframe.src;
+                session.lastSrc = iframe.src;
                 
                 if (options.local) {
                     var url = cleanIframeSrc(iframe.contentWindow.location.href);
@@ -101,17 +101,17 @@ define(function(require, exports, module) {
                 tab.className.remove("loading");
             });
             
-            session.id        = "livepreview" + counter++;
-            session.iframe    = iframe;
-            session.editor    = editor;
+            session.id = "livepreview" + counter++;
+            session.iframe = iframe;
+            session.editor = editor;
             session.transport = new PostMessage(iframe, session.id);
             
             session.transport.on("ready", function(){
-                session.transport.getSources(function(err, sources){
-                    session.styleSheets = sources.styleSheets.map(function(path){
+                session.transport.getSources(function(err, sources) {
+                    session.styleSheets = sources.styleSheets.map(function(path) {
                         return new CSSDocument(path).addTransport(session.transport);
                     });
-                    session.scripts = sources.scripts.map(function(path){
+                    session.scripts = sources.scripts.map(function(path) {
                         return new JSDocument(path).addTransport(session.transport);
                     });
                     session.html = new HTMLDocument(sources.html)
@@ -134,7 +134,7 @@ define(function(require, exports, module) {
             // Lets only destroy when the doc is destroyed
             doc.addOther(function(){ session.destroy() });
             
-            doc.on("canUnload", function(e){
+            doc.on("canUnload", function(e) {
                 var count = session.transport.getWindows().length;
                 if (count <= 1) return true;
                 
@@ -147,16 +147,16 @@ define(function(require, exports, module) {
             
             editor.container.appendChild(session.iframe);
         });
-        plugin.on("documentUnload", function(e){
-            var doc     = e.doc;
+        plugin.on("documentUnload", function(e) {
+            var doc = e.doc;
             var session = doc.getSession();
-            var iframe  = session.iframe;
+            var iframe = session.iframe;
             
             iframe.parentNode.removeChild(iframe);
             
             doc.tab.className.remove("loading");
         });
-        plugin.on("documentActivate", function(e){
+        plugin.on("documentActivate", function(e) {
             var session = e.doc.getSession();
             var path = calcRootedPath(cleanIframeSrc(session.iframe.src));
             
@@ -164,14 +164,14 @@ define(function(require, exports, module) {
             session.editor.setLocation(path, true);
             session.editor.setButtonStyle("Browser", "page_white.png");
         });
-        plugin.on("documentDeactivate", function(e){
+        plugin.on("documentDeactivate", function(e) {
             var session = e.doc.getSession();
             session.iframe.style.display = "none";
         });
-        plugin.on("navigate", function(e){
-            var tab     = plugin.activeDocument.tab;
+        plugin.on("navigate", function(e) {
+            var tab = plugin.activeDocument.tab;
             var session = plugin.activeSession;
-            var iframe  = session.iframe;
+            var iframe = session.iframe;
             var url = e.url.match(/^[a-z]\w{1,4}\:\/\//)
                 ? e.url
                 : BASEPATH + e.url;
@@ -183,19 +183,19 @@ define(function(require, exports, module) {
                 + "&_c9_host=" + (options.local ? "local" : location.origin);
             
             var path = calcRootedPath(url);
-            tab.title   = 
+            tab.title = 
             tab.tooltip = "[B] " + path;
             
             plugin.activeSession.editor.setLocation(path, true);
         });
-        plugin.on("update", function(e){
+        plugin.on("update", function(e) {
             // var iframe = plugin.activeSession.iframe;
             // if (e.saved)
             //     iframe.src = iframe.src;
         });
         plugin.on("reload", function(){
             var iframe = plugin.activeSession.iframe;
-            var tab    = plugin.activeDocument.tab;
+            var tab = plugin.activeDocument.tab;
             tab.className.add("loading");
             if (iframe.src.match(/(.*)#/))
                 iframe.src = RegExp.$1;
@@ -205,13 +205,13 @@ define(function(require, exports, module) {
             var src = plugin.activeSession.iframe.src;
             window.open(src);
         });
-        plugin.on("getState", function(e){
+        plugin.on("getState", function(e) {
             var session = e.doc.getSession();
-            var state   = e.state;
+            var state = e.state;
             
             state.currentLocation = session.currentLocation;
         });
-        plugin.on("setState", function(e){
+        plugin.on("setState", function(e) {
             var session = e.doc.getSession();
             
             if (e.state.currentLocation) {
